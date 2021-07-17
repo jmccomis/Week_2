4b72ca70c69fcc08274e0558b346e578ffe6bb47
jmccomis <87583772+jmccomis@users.noreply.github.com>
2021-07-17 17:26:39 -0500

    Add files via upload

---
+++ Keyboard_input.py
@@ -0,0 +1,13 @@
+print("Hello world")
+print("Hello Jake")
+n= int(input('How many kids do you have? enter your response here-----> '))
+print('you have ' + str(n) + ' kids')
+x = str(input('is that the correct amount? (type "yes" or "no")----> '))
+a=  str('yes')
+b = str('no')
+if x == a:
+    print ('I am glad I got it right')
+if x == b:
+    print ('I will try again')
+
+       
