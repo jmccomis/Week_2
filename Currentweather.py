89373e070dd2bb332c4f5f4b51593985db27ed00
eggebee6 <54823750+eggebee6@users.noreply.github.com>
2021-07-19 21:08:06 -0500

    Add code to demonstrate use of CurrentWeather

--- Currentweather.py
+++ Currentweather.py
@@ -8,8 +8,36 @@ class CurrentWeather:
                 return self.weather_data [self.city][1]
         def getWindSpeed(self) :
                 return self.weather_data [self.city][2]
+        def defineWeatherData(self, temperature, conditions, windSpeed):
+                self.weather_data [self.city] = [temperature, conditions, windSpeed]
+                return
+
+def printWeather(weatherData):
+        try:
+                print('City: ' + weatherData.city)
+                print('  Temperature: ' + weatherData.getTemperature())
+                print('  Conditions: ' + weatherData.getWeatherConditions())
+                print('  Wind: ' + weatherData.getWindSpeed())
+        except KeyError as err:
+                print('Error: No weather data exists for city')
                 
+        print()
+        return
 
 
+# Create class instances for 3 cities
+# Toronto and Montreal weather data are already defined in the CurrentWeather class
+# Ontario weather does not exist (yet)
+TorontoWeather = CurrentWeather('Toronto')
+MontrealWeather = CurrentWeather('Montreal')
+OntarioWeather = CurrentWeather('Ontario')
 
+# Printing data for Toronto and Montreal should work
+# Trying to print the non-existent data for Ontario should cause and exception
+printWeather(TorontoWeather)
+printWeather(MontrealWeather)
+printWeather(OntarioWeather)
 
+# Add data for Ontario and try to print again
+OntarioWeather.defineWeatherData('14', 'partly cloudy', '6 km/h W')
+printWeather(OntarioWeather)
